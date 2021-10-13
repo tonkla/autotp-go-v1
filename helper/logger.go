4f0fa213b832561ade3915fb118d1b1be922b39e
Surakarn Samkaew <tonkla@gmail.com>
2021-10-13 17:30:04 +0700

    Refactor logs

--- helper/logger.go
+++ helper/logger.go
@@ -14,16 +14,41 @@ func Logf(format string, v ...interface{}) {
 	log.Printf(format, v...)
 }
 
-func LogNewF(o *types.Order) {
+func LogNew(o *types.Order) {
 	logOpen("NEW", o)
 }
 
-func LogFilledF(o *types.Order) {
+func LogFilled(o *types.Order) {
 	logOpen("FILLED", o)
 }
 
-func LogCanceledF(o *types.Order) {
+func LogCanceled(o *types.Order) {
 	logOpen("CANCELED", o)
+}
+
+func LogNewF(o *types.Order) {
+	logOpenF("NEW", o)
+}
+
+func LogFilledF(o *types.Order) {
+	logOpenF("FILLED", o)
+}
+
+func LogCanceledF(o *types.Order) {
+	logOpenF("CANCELED", o)
+}
+
+func LogClosed(open *types.Order, close *types.Order) {
+	log := types.LogCloseOrder{
+		Action: "CLOSED",
+		Qty:    close.Qty,
+		Stop:   open.StopPrice,
+		Close:  open.ClosePrice,
+		Zone:   open.ZonePrice,
+		Open:   open.OpenPrice,
+		Profit: open.PL,
+	}
+	Log(log)
 }
 
 func LogClosedF(open *types.Order, close *types.Order) {
@@ -40,6 +65,17 @@ func LogClosedF(open *types.Order, close *types.Order) {
 }
 
 func logOpen(action string, o *types.Order) {
+	log := types.LogOpenOrder{
+		Action: action,
+		Qty:    o.Qty,
+		Open:   o.OpenPrice,
+		Zone:   o.ZonePrice,
+		TP:     o.TPPrice,
+	}
+	Log(log)
+}
+
+func logOpenF(action string, o *types.Order) {
 	log := types.LogOpenFOrder{
 		Action:  action,
 		Type:    o.Type,
