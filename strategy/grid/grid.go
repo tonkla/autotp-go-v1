b5d465f62d24771756364384769e2258a85c5534
Surakarn Samkaew <tonkla@gmail.com>
2021-10-31 13:03:33 +0700

    Fix grid: never close when apply TA

--- strategy/grid/grid.go
+++ strategy/grid/grid.go
@@ -37,20 +37,6 @@ func (s Strategy) OnTick(ticker t.Ticker) *t.TradeOrders {
 		return nil
 	}
 
-	if s.BP.ApplyTA {
-		const numberOfBars = 50
-		prices4h := s.EX.Get4hHistoricalPrices(s.BP.Symbol, numberOfBars)
-		atr4h := common.GetATR(prices4h, int(s.BP.MAPeriod))
-		if atr4h != nil && !common.IsLowerMA(ticker.Price, prices4h, s.BP.MAPeriod, 0.4**atr4h) {
-			return nil
-		}
-		prices1h := s.EX.Get1hHistoricalPrices(s.BP.Symbol, numberOfBars)
-		atr1h := common.GetATR(prices1h, int(s.BP.MAPeriod))
-		if atr1h != nil && !common.IsLowerMA(ticker.Price, prices1h, s.BP.MAPeriod, 0.4**atr1h) {
-			return nil
-		}
-	}
-
 	var openOrders, closeOrders []t.Order
 
 	qo := t.QueryOrder{
@@ -68,6 +54,48 @@ func (s Strategy) OnTick(ticker t.Ticker) *t.TradeOrders {
 	if s.BP.View == t.ViewLong || s.BP.View == t.ViewNeutral {
 		if s.BP.StartPrice > 0 && ticker.Price > s.BP.StartPrice && len(s.DB.GetActiveLimitOrders(qo)) == 0 {
 			return nil
+		}
+
+		if s.BP.GridTP > 0 {
+			o := s.DB.GetLowestFilledBuyOrder(qo)
+			if o != nil && s.DB.GetTPOrder(o.ID) == nil {
+				_tp := h.NormalizeDouble(o.ZonePrice+gridWidth*s.BP.GridTP, s.BP.PriceDigits)
+				_stop := h.CalcTPStop(t.OrderSideBuy, _tp, float64(s.BP.SLim.TPStop), s.BP.PriceDigits)
+				if ticker.Price+(_tp-_stop) > _stop {
+					tpo := t.Order{
+						ID:          h.GenID(),
+						Exchange:    s.BP.Exchange,
+						Symbol:      s.BP.Symbol,
+						BotID:       s.BP.BotID,
+						Side:        t.OrderSideSell,
+						Type:        t.OrderTypeTP,
+						Status:      t.OrderStatusNew,
+						Qty:         o.Qty,
+						OpenOrderID: o.ID,
+						OpenPrice:   _tp,
+						StopPrice:   _stop,
+					}
+					closeOrders = append(closeOrders, tpo)
+				}
+			}
+		}
+
+		if s.BP.ApplyTA {
+			const numberOfBars = 50
+			prices4h := s.EX.Get4hHistoricalPrices(s.BP.Symbol, numberOfBars)
+			atr4h := common.GetATR(prices4h, int(s.BP.MAPeriod))
+			if atr4h == nil || !common.IsLowerMA(ticker.Price, prices4h, s.BP.MAPeriod, 0.4**atr4h) {
+				return &t.TradeOrders{
+					CloseOrders: closeOrders,
+				}
+			}
+			prices1h := s.EX.Get1hHistoricalPrices(s.BP.Symbol, numberOfBars)
+			atr1h := common.GetATR(prices1h, int(s.BP.MAPeriod))
+			if atr1h == nil || !common.IsLowerMA(ticker.Price, prices1h, s.BP.MAPeriod, 0.4**atr1h) {
+				return &t.TradeOrders{
+					CloseOrders: closeOrders,
+				}
+			}
 		}
 
 		var count int64 = 0
@@ -98,30 +126,6 @@ func (s Strategy) OnTick(ticker t.Ticker) *t.TradeOrders {
 			}
 			if count++; count == s.BP.OpenZones {
 				break
-			}
-		}
-
-		if s.BP.GridTP > 0 {
-			o := s.DB.GetLowestFilledBuyOrder(qo)
-			if o != nil && s.DB.GetTPOrder(o.ID) == nil {
-				_tp := h.NormalizeDouble(o.ZonePrice+gridWidth*s.BP.GridTP, s.BP.PriceDigits)
-				_stop := h.CalcTPStop(t.OrderSideBuy, _tp, float64(s.BP.SLim.TPStop), s.BP.PriceDigits)
-				if ticker.Price+(_tp-_stop) > _stop {
-					tpo := t.Order{
-						ID:          h.GenID(),
-						Exchange:    s.BP.Exchange,
-						Symbol:      s.BP.Symbol,
-						BotID:       s.BP.BotID,
-						Side:        t.OrderSideSell,
-						Type:        t.OrderTypeTP,
-						Status:      t.OrderStatusNew,
-						Qty:         o.Qty,
-						OpenOrderID: o.ID,
-						OpenPrice:   _tp,
-						StopPrice:   _stop,
-					}
-					closeOrders = append(closeOrders, tpo)
-				}
 			}
 		}
 	}
